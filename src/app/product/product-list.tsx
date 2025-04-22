1bdab57cd9c4799d006a192990112a015ffbdfa7
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-04-22 20:37:32 +0700

    Remove all dark utility class

--- src/app/product/product-list.tsx
+++ src/app/product/product-list.tsx
@@ -27,7 +27,7 @@ const ProductList: React.FC<Props> = ({ products, lang = "en" }) => {
     <div className="p-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 place-content-center ">
       {products.map((product) => (
         <div
-          className="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+          className="max-w-sm flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm"
           key={product.id}
         >
           <a href="#">
@@ -41,16 +41,16 @@ const ProductList: React.FC<Props> = ({ products, lang = "en" }) => {
           </a>
           <div className="p-5 flex flex-col flex-1">
             <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
                 {product.name[lang]}
               </h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p className="mb-3 font-normal text-gray-700 ">
               {product.description[lang]}
             </p>
             <a
               href="#"
-              className="mt-auto w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="mt-auto w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
             >
               Read more
               <svg
