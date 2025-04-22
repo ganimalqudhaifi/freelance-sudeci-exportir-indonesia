c24118f0d7a9075a086c9c9e6eb5f950b921edcf
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-04-22 14:58:37 +0700

    Add copyright component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ContactSection from "@/components/contact-section";
+import Copyright from "@/components/copyright";
 import NavigationHeader from "@/components/navigation-header";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <NavigationHeader />
       <ContactSection />
+      <Copyright />
     </>
   );
 }
---
+++ src/components/copyright.tsx
@@ -0,0 +1,7 @@
+export default function Copyright() {
+  return (
+    <div className="bg-neutral-900 text-neutral-200 text-center py-8">
+      <span>© 2025 Sudeci Exportir Indonesia. All Rights Reserved. </span>
+    </div>
+  );
+}
