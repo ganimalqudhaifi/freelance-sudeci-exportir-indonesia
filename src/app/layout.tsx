1ddf6407250dd4a21d16330235ac5ead2524cab0
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-04-22 18:22:39 +0700

    Add default layout for navigating header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavigationHeader from "@/components/navigation-header";
+import ContactSection from "@/components/contact-section";
+import Copyright from "@/components/copyright";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavigationHeader />
         {children}
+        <ContactSection />
+        <Copyright />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import ContactSection from "@/components/contact-section";
-import Copyright from "@/components/copyright";
-import NavigationHeader from "@/components/navigation-header";
-
 export default function Home() {
-  return (
-    <>
-      <NavigationHeader />
-      <ContactSection />
-      <Copyright />
-    </>
-  );
+  return <>Home Page</>;
 }
