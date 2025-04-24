0c04f317f306960a7e61aad483fb30c2c265335d
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-04-25 00:01:15 +0700

    Remove product page and move all components

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,12 +1,3 @@
-import BannerSection from "./banner-section";
-import ProductList from "./product-list";
-import products from "@/data/products.json";
-
 export default function Page() {
-  return (
-    <>
-      <BannerSection />
-      <ProductList products={products} lang="en" />
-    </>
-  );
+  return <>Product Page</>;
 }
--- src/components/hero-header.tsx
+++ src/components/hero-header.tsx
@@ -1,18 +1,20 @@
-export default function BannerSection() {
+interface HeroHeaderProps {
+  title?: string;
+  description?: string;
+}
+
+export default function HeroHeader({
+  title = "Empowering Trade, Elevating Quality",
+  description = "Connecting local producers with global buyers through trusted export solutions.",
+}: HeroHeaderProps) {
   return (
-    <section className="bg-center bg-no-repeat bg-[url('/banner-product.png')] bg-cover bg-gray-700 bg-blend-multiply">
+    <section className="bg-center bg-no-repeat bg-[url('/banner-product.png')] bg-cover bg-gray-500 bg-blend-multiply">
       <div className="px-8 mx-auto max-w-screen-xl text-center py-20 lg:py-36">
         <h1 className="mb-8 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-5xl">
-          Our Products
+          {title}
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-          At{" "}
-          <span className="font-semibold">PT. SUDECI EXPORTIR INDONESIA </span>,
-          we specialize in sourcing and exporting premium Indonesian
-          commodities. Every product we offer is carefully selected to meet
-          international standards of quality and sustainability. We collaborate
-          with trusted local producers to ensure freshness, authenticity, and
-          traceability.
+          {description}
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <a
@@ -37,10 +39,10 @@ export default function BannerSection() {
             </svg>
           </a>
           <a
-            href="#"
+            href="#product"
             className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
           >
-            Learn more
+            Explore Our Products
           </a>
         </div>
       </div>
