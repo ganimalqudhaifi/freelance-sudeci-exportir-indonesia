4de54724676fe9c45de4858a8310cc0e58f189ba
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2025-05-06 19:41:03 +0700

    Fix href pointer in hero header component

--- src/components/hero-header.tsx
+++ src/components/hero-header.tsx
@@ -39,7 +39,7 @@ export default function HeroHeader({
             </svg>
           </a>
           <a
-            href="#product"
+            href="#products"
             className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
           >
             Explore Our Products
